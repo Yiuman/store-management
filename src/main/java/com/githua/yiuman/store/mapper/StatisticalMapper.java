8dfeff68fb43275f364070f5233d49d51a5f5de4
yiumankam <415481084@qq.com>
2021-01-06 13:46:02 +0800

    升级基础依赖

--- src/main/java/com/githua/yiuman/store/mapper/StatisticalMapper.java
+++ src/main/java/com/githua/yiuman/store/mapper/StatisticalMapper.java
@@ -1,6 +1,7 @@
 package com.githua.yiuman.store.mapper;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.githua.yiuman.store.dto.StatisticalDto;
@@ -20,7 +21,7 @@ import java.math.BigDecimal;
  */
 @Mapper
 @Repository
-public interface StatisticalMapper {
+public interface StatisticalMapper  {
 
     @Select("select count(1) from sm_product")
     Long countProducts();
